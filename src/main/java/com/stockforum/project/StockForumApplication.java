878cfa03a324428c6337b21c56707e272520811c
jaewonks <jaewonaaart@gmail.com>
2024-04-19 10:57:32 +0900

    first commit

---
+++ src/main/java/com/stockforum/project/StockForumApplication.java
@@ -0,0 +1,13 @@
+package com.stockforum.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockforum/project/StockForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockforum.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
